805c9eebac7329bdb3e31c4156adf4ceb6814630
Yajun Bai <38762233+AUSB@users.noreply.github.com>
2020-05-05 08:19:03 -0400

    Pass array to a component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import React, { Component } from 'react';
 import New1 from './New1'
+
+class App extends Component {
+    state = {
+        new1: [
+            { name: 'ryu', age: 30, belt: 'black', id: 1 },
+            { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+            { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+        ]
+    }
+    render() {
+        return (
+            <div className="App">
+                <h1>My first React app!</h1>
+                <p>Welcome to my Territory:)</p>
+                <New1 new1={this.state.new1} />
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+
+
 //import logo from './logo.svg';
 //import './App.css';
 
@@ -8,28 +33,7 @@ import New1 from './New1'
 //     <div className="App">
 //       <h1>My first React app!</h1>
 //       <p>Welcome to my Territory:)</p>
-      
+
 //     </div>
 //   );
 // }
-
-class App extends Component {
-    state = {
-        new1:[
-            { name: 'ryu', age: 30, belt: 'black', id: 1},
-            { name: 'Yoshi', age: 20, belt:'green', id: 2},
-            { name: 'Crystal', age: 25, belt: 'pink', id: 3}
-        ]
-    }
-    render() {
-        return(
-      <div className="App">
-        <h1>My first React app!</h1>
-        <p>Welcome to my Territory:)</p>
-        <New1 new1 = { this.state.new1 } /> 
-      </div>
-    );
-  }
-}
-
-export default App;
--- src/New1.js
+++ src/New1.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';　　
 
-//import logo from './logo.svg';
-//import './App.css';
+//this is a functional component, in the component, we dont use state, this. and there is no render method
+//we dont automatically get the props passed to it like we do inside a class-based component
+// we just use props as the parameter passed in the funtional component
+//we are not refering to and instance of a class(like this), 
+//we just reference the props that we pass into that function as a paramter, 
+const New1 = (props) => {  // props is the array in App.js, it is new1 in App.js
+    const { new1 } = props;//  then we can access it just use the same props
+    //const new1 = this.props.new1;
+    const newoList = new1.map(newo => {// transfer each element "newo" of array "new1" to a new elememt which consists of new array 
+        return (
+            <div className="newo" key={newo.id}>
+                <div> Name: {newo.name}</div>
+                <div> Age: {newo.age}</div>
+                <div> Belt: {newo.belt}</div>
+            </div>
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>My first React app!</h1>
-//       <p>Welcome to my Territory:)</p>
-      
-//     </div>
-//   );
-// }
-class New1 extends Component {
-    render() {
-        //const { new1 } = this.props;
-        const new1 = this.props.new1;
-        const newoList = new1.map( newo => {
-                                return(
-                                    <div className="newo" key = { newo.id }>
-                                        <div> Name: { newo.name }</div>
-                                        <div> Age: { newo.age }</div>
-                                        <div> Belt: { newo.belt }</div>
-                                    </div>
-                                )
-                                }
-                      )
-    return(
-        <div className = "newo-list">
-            { newoList}
+           
+        )
+    }
+    )
+    return (
+        <div className="newo-list">
+            {newoList}
         </div>
     )
-    }
+
 }
 export default New1;
